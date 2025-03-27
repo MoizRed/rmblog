f09c2127d0088db6faa0a4165486a0ddcb86f45b
MoizRed <abdoureduniv2000@gmail.com>
2025-03-28 00:22:30 +0100

    first

--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -1,9 +1,9 @@
 
-export default function Home() {
+export default function page() {
   return (
     <>
       <div className="flex justify-center flex-col  items-center">
-     <h1 className="sm:ml-100 sm:mr-100 text-center capitalize text-5xl" >Get The most out of everything</h1>
+     <h1 className="sm:ml-100 sm:mr-100 text-center capitalize text-5xl" >Subscribe to my monthly news letter</h1>
       <h2 className="text-center">get an insight on the latest news</h2>
       </div>
    
